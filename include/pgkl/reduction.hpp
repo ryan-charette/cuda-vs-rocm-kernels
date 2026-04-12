1607ff88ebf6be57a2779737e9d99cd45cf98fc8
Ryan Charette <100053398+ryan-charette@users.noreply.github.com>
2026-04-11 20:37:51 -0500

    Update reduction.hpp

--- include/pgkl/reduction.hpp
+++ include/pgkl/reduction.hpp
@@ -1,10 +1,10 @@
 #pragma once
 
-#include <cstddef>
 #include <vector>
 
 namespace pgkl {
 
     float reduction_cpu(const std::vector<float>& input);
+    float reduction_cuda(const std::vector<float>& input);
 
 } // namespace pgkl
