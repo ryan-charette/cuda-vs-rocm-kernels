d09a749815ff9d7cc63711461d8f3e4287dfad81
Ryan Charette <100053398+ryan-charette@users.noreply.github.com>
2026-04-16 20:05:40 -0500

    Update reduction.hpp

--- include/pgkl/reduction.hpp
+++ include/pgkl/reduction.hpp
@@ -7,5 +7,6 @@ namespace pgkl {
 [[nodiscard]] auto reduction_cpu(std::span<const float> input) -> float;
 [[nodiscard]] auto reduction_cuda(std::span<const float> input) -> float;
 [[nodiscard]] auto reduction_hip(std::span<const float> input) -> float;
+[[nodiscard]] auto reduction_sycl(std::span<const float> input) -> float;
 
 }  // namespace pgkl
