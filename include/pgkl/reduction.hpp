d3cb965c8ff2ecae34372fa0c8e82588b56c6d01
Ryan Charette <100053398+ryan-charette@users.noreply.github.com>
2026-05-23 14:24:56 -0500

    Include timing

--- include/pgkl/reduction.hpp
+++ include/pgkl/reduction.hpp
@@ -2,11 +2,13 @@
 
 #include <span>
 
+#include "pgkl/timing.hpp"
+
 namespace pgkl {
 
-[[nodiscard]] auto reduction_cpu(std::span<const float> input) -> float;
-[[nodiscard]] auto reduction_cuda(std::span<const float> input) -> float;
-[[nodiscard]] auto reduction_hip(std::span<const float> input) -> float;
-[[nodiscard]] auto reduction_sycl(std::span<const float> input) -> float;
+[[nodiscard]] auto reduction_cpu(std::span<const float> input, TimingResult* timing = nullptr) -> float;
+[[nodiscard]] auto reduction_cuda(std::span<const float> input, TimingResult* timing = nullptr) -> float;
+[[nodiscard]] auto reduction_hip(std::span<const float> input, TimingResult* timing = nullptr) -> float;
+[[nodiscard]] auto reduction_sycl(std::span<const float> input, TimingResult* timing = nullptr) -> float;
 
 }  // namespace pgkl
